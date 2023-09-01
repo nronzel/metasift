7e38b7894658fff777b2176d3840d9ee11381843
nronzel <86695181+nronzel@users.noreply.github.com>
2023-09-01 13:52:17 -0400

    feat: batch processing for .docx metadata extraction

--- document/document.py
+++ document/document.py
@@ -1,5 +1,8 @@
 from models.metadata_extractor import MetadataExtractor
 from models.password_unlocker import PasswordUnlocker
+from utils.helpers import is_supported_filetype
+
+import os
 
 
 class Document:
@@ -9,8 +12,28 @@ class Document:
 
     def extract_metadata(self):
         metadata_extractor = MetadataExtractor(self.path)
-        data = metadata_extractor.extract()
-        return data
+        if self.type == "filepath":
+            return metadata_extractor.extract()
+        elif self.type == "directory":
+            return self._extract_directory_metadata()
+
+    def _extract_directory_metadata(self):
+        metadata = {}
+        supported_files = self._crawl_for_supported_files()
+        if supported_files is not None:
+            for file in supported_files:
+                title = file.split("/")[-1]
+                filename, extension = os.path.splitext(title)
+
+                # until other formats are supported, leave this in.
+                if extension != ".docx":
+                    print(f"\n'{extension}' not yet supported for metadata extraction.")
+                    continue
+
+                extractor = MetadataExtractor(file)
+                data = extractor.extract()
+                metadata[filename] = data
+        return metadata
 
     def clean_metadata(self):
         # CleanMetadata here
@@ -22,3 +45,20 @@ class Document:
 
     def set_type(self, type):
         self.type = type
+
+    def _crawl_for_supported_files(self):
+        supported_files = []
+        directory = os.path.abspath(self.path)
+
+        if not os.path.exists(directory):
+            print("\nThe specified directory does not exist.")
+            return
+
+        for filename in os.listdir(directory):
+            filepath = os.path.join(directory, filename)
+
+            if os.path.isfile(filepath):
+                if is_supported_filetype(filename):
+                    supported_files.append(filepath)
+
+        return supported_files
