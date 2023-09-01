07ab8259296c7da05072e7a51c17eee249fbbb58
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-31 21:33:16 -0400

    refactor: removed filetype selection menu
    didn't need to ask for filetype, instead there is a validation function
    that will validate whether the supplied file is supported or not.

--- cli/cli.py
+++ cli/cli.py
@@ -35,54 +35,48 @@ class CLI:
     def draw_main_menu(self):
         self.state = State.MAIN_MENU
         options = {
-            "1": {
-                "text": "Extract metadata",
-                "action": self.set_action_and_goto_file_selection("extract_metadata"),
-            },
-            "2": {
-                "text": "Clean metadata *coming soon",
-                "action": self.set_action_and_goto_file_selection("clean_metadata"),
-            },
+            "1": {"text": "Extract metadata", "action": self.extract_metadata},
+            "2": {"text": "Clean metadata *coming soon", "action": self.clean_metadata},
             "3": {
                 "text": "Remove password protection (.docx only)",
-                "action": self.perform_unlock_docx,
+                "action": self.unlock_docx,
             },
         }
         self.draw_menu("Main Menu", options)
         self.handle_choice(options)
 
-    def set_action_and_goto_file_selection(self, action):
-        def wrapper():
-            self.action = action
-            self.goto_filetype_selection()
+    # def set_action_and_goto_file_selection(self, action):
+    #     def wrapper():
+    #         self.action = action
+    #         self.goto_filetype_selection()
+    #
+    #     return wrapper
 
-        return wrapper
+    # def perform_unlock_docx(self):
+    #     self.action = "remove_password"
+    #     self.perform_action()
+    #
+    # def goto_filetype_selection(self):
+    #     self.state = State.FILETYPE_SELECTION
+    #     self.draw_filetype_selection_menu()
 
-    def perform_unlock_docx(self):
-        self.action = "remove_password"
-        self.perform_action()
+    # def draw_filetype_selection_menu(self):
+    #     options = {
+    #         "1": {"text": ".docx", "action": self.perform_action},
+    #         "2": {"text": ".pdf (*coming soon)", "action": None},
+    #         "3": {"text": ".jpg or .png (*coming soon)", "action": None},
+    #         "4": {"text": "Main Menu", "action": self.draw_main_menu},
+    #     }
+    #     self.draw_menu("Filetype", options)
+    #     self.handle_choice(options)
 
-    def goto_filetype_selection(self):
-        self.state = State.FILETYPE_SELECTION
-        self.draw_filetype_selection_menu()
-
-    def draw_filetype_selection_menu(self):
-        options = {
-            "1": {"text": ".docx", "action": self.perform_action},
-            "2": {"text": ".pdf (*coming soon)", "action": None},
-            "3": {"text": ".jpg or .png (*coming soon)", "action": None},
-            "4": {"text": "Main Menu", "action": self.draw_main_menu},
-        }
-        self.draw_menu("Filetype", options)
-        self.handle_choice(options)
-
-    def perform_action(self):
-        if self.action == "extract_metadata":
-            self.extract_metadata()
-        elif self.action == "clean_metadata":
-            self.clean_metadata()
-        elif self.action == "remove_password":
-            self.unlock_docx()
+    # def perform_action(self):
+    #     if self.action == "extract_metadata":
+    #         self.extract_metadata()
+    #     elif self.action == "clean_metadata":
+    #         self.clean_metadata()
+    #     elif self.action == "remove_password":
+    #         self.unlock_docx()
 
     def extract_metadata(self):
         self.document = Document()
@@ -112,11 +106,7 @@ class CLI:
     def run(self):
         self.intro()
         while True:
-            match self.state:
-                case State.MAIN_MENU:
-                    self.draw_main_menu()
-                case State.FILETYPE_SELECTION:
-                    self.draw_filetype_selection_menu()
+            self.draw_main_menu()
 
     def intro(self):
         print(
