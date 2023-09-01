d62a955476728301ef9369f764238e6e8b0aa271
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-31 21:12:08 -0400

    feat: tests for new helpers
    fixed old tests to work with new func name

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from utils.helpers import filename_or_directory
+from utils.helpers import is_valid_filename, is_supported_filetype
 
 
 # Helpers
@@ -7,13 +7,13 @@ class TestHelpers(unittest.TestCase):
     def test_filename_or_directory(self):
         values = [
             "test.docx",
-            "test.doc",
+            "test.pdf",
             "test.jpg",
             "test.png",
         ]
 
         for value in values:
-            result = filename_or_directory(value)
+            result = is_valid_filename(value)
             self.assertTrue(result, f"Expected {value} to be True")
 
     def test_bad_filename_or_directory(self):
@@ -25,7 +25,19 @@ class TestHelpers(unittest.TestCase):
             "12315",
         ]
         for value in values:
-            result = filename_or_directory(value)
+            result = is_valid_filename(value)
+            self.assertFalse(result, f"Expected {value} to be False")
+
+    def test_supported_extensions(self):
+        values = ["test.docx", "test.pdf", "test.jpg", "test.png"]
+        for value in values:
+            result = is_supported_filetype(value)
+            self.assertTrue(result, f"Expected {value} to be True")
+
+    def test_unsupported_extensions(self):
+        values = ["test.doc", "test.gif", "test.avi"]
+        for value in values:
+            result = is_supported_filetype(value)
             self.assertFalse(result, f"Expected {value} to be False")
 
 
