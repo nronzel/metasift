154501c8f6b3cdf0ddc801494346c3a73d72d4e7
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-31 21:34:21 -0400

    refactor: validate supplied input is valid filename or directory

--- document/document.py
+++ document/document.py
@@ -1,5 +1,8 @@
 from models.metadata_extractor import MetadataExtractor
 from models.password_unlocker import PasswordUnlocker
+from utils.helpers import is_supported_filetype, is_valid_filename
+
+import os
 
 
 class Document:
@@ -8,6 +11,8 @@ class Document:
             self.path = path
         else:
             self.set_path()
+
+        self.type = None
 
     def extract_metadata(self):
         metadata_extractor = MetadataExtractor(self.path)
@@ -23,5 +28,19 @@ class Document:
         return password_unlocker.unlock()
 
     def set_path(self):
-        path = input("Please provide a path or filename: ")
-        self.path = path
+        while True:
+            path = input("Please provide a path or filename: ")
+            is_file = is_valid_filename(path)
+            is_valid_directory = os.path.isdir(path)
+
+            if is_file and is_supported_filetype(path):
+                self.type = "filename"
+                self.path = path
+                break
+            else:
+                print("Invalid filename provided. Try again.")
+
+            if is_valid_directory:
+                self.type = "directory"
+                self.path = path
+                break
