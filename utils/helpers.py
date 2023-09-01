94217addcec5136f9ece5f5ae226dae2f9b57e13
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-31 21:11:49 -0400

    refactor: split out logic

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,10 +1,28 @@
 import re
+import os
 
 
-def filename_or_directory(text):
-    """
-    True -> filename
-    False -> directory
-    """
-    pattern = r"\.[a-zA-Z]{3,4}$"  # does the text end in . with 3 letters
-    return re.search(pattern, text)
+def is_valid_filename(text):
+    pattern = r".*\.[a-zA-Z]{3,4}$"
+    file = bool(re.match(pattern, text))
+    if file:
+        is_supported = is_supported_filetype(text)
+        if not is_supported:
+            print("Filetype not supported, please try again.")
+        else:
+            return True
+    else:
+        return False
+
+
+def is_supported_filetype(text):
+    supported_extensions = [
+        ".docx",
+        ".pdf",
+        ".jpg",
+        ".png",
+    ]
+    _, extension = os.path.splitext(text.strip())
+    if extension.lower() in supported_extensions:
+        return True
+    return False
