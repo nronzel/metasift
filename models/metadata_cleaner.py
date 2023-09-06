73cb3b4dc9f8115dbfdd5ca3407cd5e5a5497376
nronzel <86695181+nronzel@users.noreply.github.com>
2023-09-06 12:05:50 -0400

    started laying out cleaner logic

--- models/metadata_cleaner.py
+++ models/metadata_cleaner.py
@@ -1,7 +1,33 @@
-class MetadataCleaner:
+import os
+
+
+class Cleaner:
+    def clean(self):
+        raise NotImplementedError
+
+
+class MetadataCleaner(Cleaner):
+    def __init__(self, metadata):
+        self.metadata = metadata
+        self.docx_cleaner = DocxCleaner(self.metadata)
+
+    def clean(self):
+        # cleaning logic
+        pass
+
+
+class DocxCleaner(Cleaner):
     def __init__(self, metadata):
         self.metadata = metadata
 
     def clean(self):
-        # cleaning logic
+        pass
+
+
+class Editor:
+    def __init__(self, data, namespaces):
+        self.data = data
+        self.namespaces = namespaces
+
+    def edit(self, field, value):
         pass
