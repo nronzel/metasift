c658a8185b043723590484c0a623d0818669073a
nronzel <86695181+nronzel@users.noreply.github.com>
2023-09-13 17:48:25 -0400

    refactor: loops over provided file(s)

--- models/password_unlocker.py
+++ models/password_unlocker.py
@@ -13,49 +13,30 @@ class PasswordUnlocker:
     Removes password protection from .docx files.
     """
 
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, files):
+        self.files = files
 
     def unlock(self):
-        if os.path.isdir(self.path):
-            return self._batch_unlock()
-        elif os.path.isfile(self.path):
-            return self._unlock_file()
-        else:
-            color_print("red", "\nInvalid path or filename provided. Try again.")
+        for file in self.files:
+            temp_dir = "temp_docx"
+            output_dir = "unlocked-documents"
+            new_docx_file = "unlocked_" + file.split("/")[-1]
+            new_docx_filepath = os.path.join(output_dir, new_docx_file)
 
-    def _unlock_file(self):
-        temp_dir = "temp_docx"
-        output_dir = "unlocked-documents"
-        new_docx_file = "unlocked_" + self.path.split(os.path.sep)[-1]
-        new_docx_filepath = os.path.join(output_dir, new_docx_file)
-
-        if not DirectoryHandler.ensure_directory_exists(output_dir):
-            print("\nFailed to ensure directory exists.")
-            return
-
-        try:
-            color_print("yellow", f"{('-') * 40}")
-            FileHandler.unzip_files(self.path, temp_dir)
-            if not SettingsModifier.modify_settings(temp_dir):
-                color_print("cyan", "No protection found! No action required.")
+            if not DirectoryHandler.ensure_directory_exists(output_dir):
+                print("\nFailed to ensure directory exists.")
                 return
-            FileHandler.zip_file(temp_dir, new_docx_filepath)
-        finally:
-            CleanupHandler.cleanup(temp_dir)
 
-        color_print(
-            "cyan", f"\nUnlocked documents can be found at {new_docx_filepath}\n"
-        )
+            try:
+                color_print("yellow", f"{('-') * 40}")
+                FileHandler.unzip_files(file, temp_dir)
+                if not SettingsModifier.modify_settings(temp_dir):
+                    color_print("cyan", "No protection found! No action required.")
+                    return
+                FileHandler.zip_file(temp_dir, new_docx_filepath)
+            finally:
+                CleanupHandler.cleanup(temp_dir)
 
-    def _batch_unlock(self):
-        # find all .docx files and add to list
-        docx_files = glob.glob(os.path.join(self.path, "*.docx"))
-
-        if not docx_files:
-            color_print("red", "\nNo .docx files found in the directory. Try again.\n")
-            return
-
-        for docx_file in docx_files:
-            self.path = docx_file
-            self.unlock()
+            color_print(
+                "cyan", f"\nUnlocked documents can be found at {new_docx_filepath}\n"
+            )
